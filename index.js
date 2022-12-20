d60694d60a53de142914537499bcf7e79ca34045
Adarsh Das <adarsh.191605@ncit.edu.np>
2022-12-20 15:00:21 +0545

    added turn servers

--- index.js
+++ index.js
@@ -1,63 +1,60 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const {v4:uuidv4} = require('uuid')
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidv4 } = require("uuid");
 
-app.set('view engine' ,'ejs')
+app.set("view engine", "ejs");
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{
-  res.redirect(`/${uuidv4()}`)
-})
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
 
-app.get('/:room',(req,res)=>{
-  res.render('room',{
-    roomId:req.params.room
-  })
-})
+app.get("/:room", (req, res) => {
+  res.render("room", {
+    roomId: req.params.room,
+  });
+});
 
-let allConnections =[];
-let otherPeers = []
+let allConnections = [];
+let otherPeers = [];
 
-io.on('connection',socket=>{
-  console.log(`SocketId: ${socket.id}`)
+io.on("connection", (socket) => {
+  console.log(`SocketId: ${socket.id}`);
 
-  socket.on('userconnect',(data)=>{
-
+  socket.on("userconnect", (data) => {
     console.log(data);
 
-    otherPeers = allConnections.filter( ac => ac.roomId == data.roomId )
+    otherPeers = allConnections.filter((ac) => ac.roomId == data.roomId);
 
     allConnections.push({
-      connectId:socket.id,
-      displayName:data.displayName,
-      roomId:data.roomId
-    })
+      connectId: socket.id,
+      displayName: data.displayName,
+      roomId: data.roomId,
+    });
 
-    otherPeers.forEach( p => {
-      socket.to(p.connectId).emit('inform_others_about_me',{
-        connectId:socket.id,
-        displayName:data.displayName
-      })
-    })
+    otherPeers.forEach((p) => {
+      socket.to(p.connectId).emit("inform_others_about_me", {
+        connectId: socket.id,
+        displayName: data.displayName,
+      });
+    });
 
-    socket.on('SDP_Process', (data) => {
-      socket.to(data.to_connectId).emit('SDP_Process',{
-        message:data.message,
-        from_connectId:socket.id
-      })
-    })
+    socket.on("SDP_Process", (data) => {
+      socket.to(data.to_connectId).emit("SDP_Process", {
+        message: data.message,
+        from_connectId: socket.id,
+      });
+    });
 
-    socket.emit('inform_me_about_others',otherPeers)
+    socket.emit("inform_me_about_others", otherPeers);
+  });
+});
 
-  })
-})
-
-server.listen(port,()=>{
-  console.log(`Server started on port ${port} 🚀`)
-})
-
+server.listen(port, () => {
+  console.log(`Server started on port ${port} 🚀`);
+});
--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,26 @@
-const socket = io.connect();
-const servers = {
+
+// ==================================CONSTANT DECLARATION================================
+const ICE = {
   iceServers: [
-    { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:stun1.l.google.com:19302" },
-  ],
+    {
+      urls: "stun:relay.metered.ca:80",
+    },
+    {
+      urls: "turn:relay.metered.ca:80",
+      username: "40af9eba614baed87dbab437",
+      credential: "LMuAOvhUOAIGDOdt",
+    },
+    {
+      urls: "turn:relay.metered.ca:443",
+      username: "40af9eba614baed87dbab437",
+      credential: "LMuAOvhUOAIGDOdt",
+    },
+    {
+      urls: "turn:relay.metered.ca:443?transport=tcp",
+      username: "40af9eba614baed87dbab437",
+      credential: "LMuAOvhUOAIGDOdt",
+    },
+]
 };
 
 let peerConnection;
@@ -22,6 +39,304 @@ const videoContainer = document.querySelector("#video-container");
 
 const videoStates = { None: 0, Camera: 1, Screen: 2 };
 let videoState = videoStates.None;
+
+// =======================================================================================
+
+
+// ==================================SOCKET IO============================================
+
+const socket = io.connect();
+
+socket.on("connect", () => {
+  socket.emit("userconnect", {
+    displayName,
+    roomId,
+  })
+})
+
+socket.on("inform_others_about_me", async (data) => {
+  try {
+    await createConnection(data.connectId)
+    addPeer(data.displayName, data.connectId)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+socket.on("inform_me_about_others", async (otherPeers) => {
+  try {
+    if (otherPeers) {
+      for (let i = 0; i < otherPeers.length; i++) {
+        await createConnection(otherPeers[i].connectId)
+        addPeer(otherPeers[i].displayName, otherPeers[i].connectId)
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+
+//-----"RECEIVE"  answer||offer||ICE from the server-------
+
+socket.on("SDP_Process", async (data) => {
+  try {
+    await clientProcess(data.message, data.from_connectId);
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+// ========================================================================================
+
+
+//-----"SEND"  answer||offer||ICE to the server-------
+
+const serverProcess = (data, to_connectId) => {
+  socket.emit("SDP_Process", {
+    message: data,
+    to_connectId,
+  });
+};
+
+
+//-------------Add new Peer to the DOM----------
+
+const addPeer = (displayName, connectId) => {
+  const divText = `<video class="remoteVideoPlayer" id="v_${connectId}" autoplay playsinline muted></video>
+                  <p>${displayName}</p>
+                  <audio id="a_${connectId}" muted autoplay></audio>`;
+  const videoDiv = document.createElement("div");
+  videoDiv.classList.add("remoteVideo");
+  videoDiv.innerHTML = divText;
+  videoContainer.append(videoDiv);
+  videoDiv.addEventListener("click", () => {
+    document.getElementById(`v_${connectId}`).requestFullscreen();
+  });
+};
+
+// ==================================PEER CONNECTION============================================
+
+
+//---------------------------Created a new webRTC connection-----------------------------------
+
+const createConnection = async (connectId) => {
+  try {
+    peerConnection = new RTCPeerConnection(ICE);
+
+    peerConnection.onnegotiationneeded = async (event) => {
+      await createOffer(connectId);
+    }
+
+    peerConnection.onicecandidate = (event) => {
+      if (event.candidate) {
+        serverProcess(
+          JSON.stringify({
+            icecandidate: event.candidate,
+          }),
+          connectId
+        );
+      }
+    };
+
+    //-------------"RECEIVE" remote VIDEO from other Peers----------
+
+    peerConnection.ontrack = (event) => {
+      if (!remoteVidStream[connectId]) {
+        remoteVidStream[connectId] = new MediaStream();
+      }
+      if (!remoteAudStream[connectId]) {
+        remoteAudStream[connectId] = new MediaStream();
+      }
+
+      if (event.track.kind == "video") {
+        remoteVidStream[connectId].getVideoTracks().forEach((track) => {
+          remoteVidStream[connectId].removeTrack(track);
+        });
+        remoteVidStream[connectId].addTrack(event.track);
+
+        const remoteVideoPlayer = document.getElementById(`v_${connectId}`);
+        remoteVideoPlayer.srcObject = null;
+        remoteVideoPlayer.srcObject = remoteVidStream[connectId];
+      }
+
+    //-------------"RECEIVE" remote AUDIO from other Peers----------
+
+      if (event.track.kind == "audio") {
+        remoteAudStream[connectId].getAudioTracks().forEach((track) => {
+          remoteAudStream[connectId].removeTrack(track);
+        });
+        remoteAudStream[connectId].addTrack(event.track);
+        const remoteAudioPlayer = document.getElementById(`a_${connectId}`);
+        remoteAudioPlayer.srcObject = null;
+        remoteAudioPlayer.srcObject = remoteAudStream[connectId];
+      }
+    };
+
+
+    if (videoState == videoStates.Camera || videoState == videoStates.Screen) {
+      if (webcamTracks) {
+        updateMediaSenders(webcamTracks, rtp_video_senders);
+      }
+    }
+
+    peers_ConnectionIds[connectId] = connectId;
+    peers_Connection[connectId] = peerConnection;
+
+    return peerConnection;
+  } catch (error) {
+    console.log(error);
+  }
+}
+//---------------------------------------------------------------------------------------
+
+
+//-------------Create Offer------------------
+
+const createOffer = async (connectId) => {
+  try {
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+
+    serverProcess(
+      JSON.stringify({
+        offer: peerConnection.localDescription,
+      }),
+      connectId
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+//-----Process the answer||offer||ICE from other peers comming through the server-------
+
+const clientProcess = async (message, from_connectId) => {
+  try {
+    message = JSON.parse(message);  
+
+    if (message.answer) {  //ANSWER
+      if(!peers_Connection[from_connectId].currentRemoteDescription){
+        peers_Connection[from_connectId].setRemoteDescription(message.answer)
+      }
+    } else if (message.offer) { //OFFER
+      if(!peers_Connection[from_connectId]){
+        await createConnection(from_connectId)
+      }
+
+      await peers_Connection[from_connectId].setRemoteDescription(
+        message.offer
+      );
+      if (!peers_Connection[from_connectId]) {
+        await createConnection(from_connectId);
+      }
+      const answer = await peers_Connection[from_connectId].createAnswer();
+      await peers_Connection[from_connectId].setLocalDescription(answer);
+
+      serverProcess(
+        JSON.stringify({
+          answer: answer,
+        }),
+        from_connectId
+      );
+    } else if (message.icecandidate) { //ICECANDIDATE
+      if (!peers_Connection[from_connectId]) {
+        await createConnection(from_connectId);
+      }
+      try {
+        console.log("peer",peers_Connection[from_connectId]);
+        console.log("ice",message.icecandidate);
+
+        peers_Connection[from_connectId].addIceCandidate(message.icecandidate);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// ==========================================================================================
+
+// ==================================VIDEO STREAM============================================
+
+
+//-------------Add local video to the DOM----------
+
+const videoProcess = async (newVideoState) => {
+  let localVideoStream = null;
+  try {
+    if (newVideoState == videoStates.Camera) {
+      localVideoStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+      });
+    }
+    if (newVideoState == videoStates.Screen) {
+      localVideoStream = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+        audio: false,
+      });
+    }
+
+    if (localVideoStream && localVideoStream.getVideoTracks().length > 0) {
+      webcamTracks = localVideoStream.getVideoTracks()[0];
+
+      if (webcamTracks) {
+        localVideoPlayer.srcObject = new MediaStream([webcamTracks]);
+        updateMediaSenders(webcamTracks, rtp_video_senders);
+
+        videoState = newVideoState;
+      }
+    }
+  } catch (error) {
+    console.log(error)
+    return
+  }
+};
+
+
+//-------------"SEND" local video and audio to other Peers----------
+
+const updateMediaSenders = async (track, rtp_senders) => {
+  try {
+    for (let id in peers_Connection) {
+      if (checkConnection(peers_Connection[id])) {
+        if (rtp_senders[id] && rtp_senders[id].track) {
+          rtp_senders[id].replaceTrack(track);
+        } else {
+          rtp_senders[id] = peers_Connection[id].addTrack(track);
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const checkConnection = async (connection) => {
+  try {
+    if (
+      (connection && connection.conectionState == "new") ||
+      connection.conectionState == "connecting" ||
+      connection.conectionState == "connected"
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+// ============================================================================================
+
+// ==================================EVENT HANDLERS============================================
+
 
 const micButton = document.querySelector(".fa-microphone");
 micButton.addEventListener("click", async () => {
@@ -70,244 +385,5 @@ screenButton.addEventListener("click", async () => {
   }
 });
 
-socket.on("connect", () => {
-  socket.emit("userconnect", {
-    displayName,
-    roomId,
-  });
-});
 
-socket.on("inform_others_about_me", async (data) => {
-  try {
-    addPeer(data.displayName, data.connectId);
-    await createConnection(data.connectId);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-socket.on("inform_me_about_others", async (otherPeers) => {
-  try {
-    if (otherPeers) {
-      for (let i = 0; i < otherPeers.length; i++) {
-        addPeer(otherPeers[i].displayName, otherPeers[i].connectId);
-        await createConnection(otherPeers[i].connectId);
-      }
-    }
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-socket.on("SDP_Process", async (data) => {
-  try {
-    await clientProcess(data.message, data.from_connectId);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-const addPeer = (displayName, connectId) => {
-  const divText = `<video class="remoteVideoPlayer" id="v_${connectId}" autoplay playsinline muted></video>
-                  <p>${displayName}</p>
-                  <audio id="a_${connectId}" muted autoplay></audio>`;
-  const videoDiv = document.createElement("div");
-  videoDiv.classList.add("remoteVideo");
-  videoDiv.innerHTML = divText;
-  videoContainer.append(videoDiv);
-  videoDiv.addEventListener("click", () => {
-    document.getElementById(`v_${connectId}`).requestFullscreen();
-  });
-};
-
-const createConnection = async (connectId) => {
-  try {
-    peerConnection = new RTCPeerConnection(servers);
-    peerConnection.onnegotiationneeded = async (event) => {
-      await createOffer(connectId);
-    };
-
-    peerConnection.onicecandidate = (event) => {
-      if (event.candidate) {
-        serverProcess(
-          JSON.stringify({
-            icecandidate: event.candidate,
-          }),
-          connectId
-        );
-      }
-    };
-
-    peerConnection.ontrack = (event) => {
-
-      if (!remoteVidStream[connectId]) {
-        remoteVidStream[connectId] = new MediaStream();
-      }
-      if (!remoteAudStream[connectId]) {
-        remoteAudStream[connectId] = new MediaStream();
-      }
-
-      if (event.track.kind == "video") {
-        remoteVidStream[connectId].getVideoTracks().forEach((track) => {
-          remoteVidStream[connectId].removeTrack(track)
-        });
-        remoteVidStream[connectId].addTrack(event.track)
-
-        const remoteVideoPlayer = document.getElementById(`v_${connectId}`)
-        remoteVideoPlayer.srcObject = null
-        remoteVideoPlayer.srcObject = remoteVidStream[connectId];
-
-      }
-
-      if (event.track.kind == "audio") {
-        remoteAudStream[connectId].getAudioTracks().forEach((track) => {
-          remoteAudStream[connectId].removeTrack(track);
-        });
-        remoteAudStream[connectId].addTrack(event.track);
-        const remoteAudioPlayer = document.getElementById(`a_${connectId}`);
-        remoteAudioPlayer.srcObject = null;
-        remoteAudioPlayer.srcObject = remoteAudStream[connectId];
-      }
-    };
-
-    if (videoState == videoStates.Camera || videoState == videoStates.Screen) {
-      if (webcamTracks) {
-        updateMediaSenders(webcamTracks, rtp_video_senders);
-      }
-    }
-
-    peers_ConnectionIds[connectId] = connectId
-    peers_Connection[connectId] = peerConnection
-
-    return peerConnection
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const createOffer = async (connectId) => {
-  try {
-    const offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(offer);
-
-    serverProcess(
-      JSON.stringify({
-        offer: peerConnection.localDescription,
-      }),
-      connectId
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const serverProcess = (data, to_connectId) => {
-  socket.emit("SDP_Process", {
-    message: data,
-    to_connectId,
-  });
-};
-
-const clientProcess = async (message, from_connectId) => {
-  try {
-    message = JSON.parse(message);
-
-    if (message.answer) {
-      await peers_Connection[from_connectId].setRemoteDescription(
-        new RTCSessionDescription(message.answer)
-      );
-    } else if (message.offer) {
-      await peers_Connection[from_connectId].setRemoteDescription(
-        new RTCSessionDescription(message.offer)
-      );
-      if (!peers_Connection[from_connectId]) {
-        await createConnection(from_connectId);
-      }
-      const answer = await peers_Connection[from_connectId].createAnswer();
-      await peers_Connection[from_connectId].setLocalDescription(answer);
-
-      serverProcess(
-        JSON.stringify({
-          answer: answer,
-        }),
-        from_connectId
-      );
-    } else if (message.icecandidate) {
-      if (!peers_Connection[from_connectId]) {
-        await createConnection(from_connectId);
-      }
-      try {
-        peers_Connection[from_connectId].addIceCandidate(message.icecandidate);
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  } catch (error) {
-    console.log(error)
-  }
-};
-
-const videoProcess = async (newVideoState) => {
-  let localVideoStream = null;
-  try {
-    if (newVideoState == videoStates.Camera) {
-      localVideoStream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: false,
-      });
-    }
-    if (newVideoState == videoStates.Screen) {
-      localVideoStream = await navigator.mediaDevices.getDisplayMedia({
-        video: true,
-        audio: false,
-      })
-    }
-
-    if (localVideoStream && localVideoStream.getVideoTracks().length > 0) {
-      webcamTracks = localVideoStream.getVideoTracks()[0];
-
-      if (webcamTracks) {
-        localVideoPlayer.srcObject = new MediaStream([webcamTracks])
-        await updateMediaSenders(webcamTracks, rtp_video_senders)
-
-        videoState = newVideoState;
-
-      }
-    }
-  } catch (error) {
-    console.log(error)
-  }
-
-};
-
-const updateMediaSenders = async (track, rtp_senders) => {
-  try {
-    for (let id in peers_Connection) {
-      if (checkConnection(peers_Connection[id])) {
-        if (rtp_senders[id] && rtp_senders[id].track) {
-          rtp_senders[id].replaceTrack(track);
-        } else {
-          rtp_senders[id] = peers_Connection[id].addTrack(track)
-        }
-      }
-    }
-  } catch (error) {
-    console.log(error)
-  }
-};
-
-const checkConnection = async (connection) => {
-  try {
-    if (
-      (connection && connection.conectionState == "new") ||
-      connection.conectionState == "connecting" ||
-      connection.conectionState == "connected"
-    ) {
-      return true
-    } else {
-      return false
-    }
-  } catch (error) {
-    console.log(error)
-  }
-}
+// ==========================================================================================
